6b93cd793d85a5f3ae60d21feca6bd32a642f9aa
Dany King <danielkkoper@gmail.com>
2023-01-02 16:13:07 +0100

    install npm express, create nodeJSServer.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// const getDomain = document.querySelector('.enter_dns');
-// const { value } = getDomain;
-// const showIP = document.querySelector('.show_ip');
-// const btn = document.querySelector('.btn');
-
 import dnsToIP from "./module.js";
 
 const URL = 'www.wp.pl';
