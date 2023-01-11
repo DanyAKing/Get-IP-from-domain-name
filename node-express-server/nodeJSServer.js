69b010b25c8852cd20b5a81750f0424bd6600889
Dany King <danielkkoper@gmail.com>
2023-01-11 10:56:07 +0100

    modify nodeJSServer.js

--- node-express-server/nodeJSServer.js
+++ node-express-server/nodeJSServer.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { response } = require('../src/index');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(response);
+  res.send('Server NodeJS Express')
 });
 
-app.listen(3000, () => {
-  console.log('Server working, listening on port 3000');
+app.listen(3000, '127.0.0.1', () => {
+  console.log('Server working, visit 127.0.0.1:3000.');
 });
