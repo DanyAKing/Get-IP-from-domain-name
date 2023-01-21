53c0f6a7df1220ff331f369c88bfbb529b171703
Dany King <danielkkoper@gmail.com>
2023-01-21 02:59:02 +0100

    fixed return element form function (return single element)

--- js/lookup_dns_app.js
+++ js/lookup_dns_app.js
@@ -1,13 +1,14 @@
 const { lookup } = require('dns').promises;
 
-async function checkDomain(url) {
+async function checkDomain(url, target) {
   try {
     const data = await lookup(url);
     const obj = {
       address: data.address,
       family: data.family,
     };
-    return obj;
+    const item = target.push(obj);
+    return item;
   } catch (error) {
     error.code === 'ENOTFOUND' ? console.log(`Domain not found -> ${error.hostname}`) : error.message;
     return error;
